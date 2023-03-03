f326a132681ab9b3a3532632f7a247cb22885970
Mahmoud Kamal Fawzy <65209396+mahmoud947@users.noreply.github.com>
2023-03-03 23:59:57 +0200

    config jitpack publish

--- app/src/main/java/com/example/easyconnectivity/MainActivity.kt
+++ app/src/main/java/com/example/easyconnectivity/MainActivity.kt
@@ -2,7 +2,6 @@ package com.example.easyconnectivity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.lifecycleScope
 import com.example.easy_connectivity.EasyConnectivity
@@ -11,43 +10,33 @@ import com.example.easyconnectivity.databinding.ActivityMainBinding
 import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
-    lateinit var binding:ActivityMainBinding
-    lateinit var networkMonitor: EasyConnectivity
+    lateinit var binding: ActivityMainBinding
+
+    private lateinit var easyConnectivity: EasyConnectivity
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        networkMonitor = EasyConnectivity.Builder(context = this)
+        easyConnectivity = EasyConnectivity.Builder(context = this)
             .setAcceptedHttpCodes(listOf(200))
-            .setConnectionTimeOut(1000)
             .build()
 
-//        lifecycleScope.launchWhenStarted {
-//
-//            networkMonitor.networkState.collect{
-//                Snackbar.make(this@MainActivity,binding.root,"Network status is ${it.name}",Snackbar.LENGTH_SHORT).show()
-//            }
-//        }
-//
-        networkMonitor.callBack(object :NetworkMonitorCallback{
+        easyConnectivity.callBack(object : NetworkMonitorCallback {
             override fun onAvailable() {
-                Snackbar.make(this@MainActivity,binding.root,"Network status is onAvailable",Snackbar.LENGTH_SHORT).show()
+                // connected
             }
 
             override fun onUnAvailable() {
-                Snackbar.make(this@MainActivity,binding.root,"Network status is onUnAvailable",Snackbar.LENGTH_SHORT).show()
+                // not connected
             }
 
             override fun onLosing() {
-                Snackbar.make(this@MainActivity,binding.root,"Network status is onLosing",Snackbar.LENGTH_SHORT).show()
+                // Losing connect
             }
 
             override fun onLost() {
-                Snackbar.make(this@MainActivity,binding.root,"Network status is onLost}",Snackbar.LENGTH_SHORT).show()
+                // lost connect
             }
-
-
         })
-
     }
 }
